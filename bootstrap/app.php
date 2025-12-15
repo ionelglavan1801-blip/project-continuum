aa5292315728e71053a9e56f6927727399378052
IonG <ionelglavan1801@gmail.com>
2025-12-15 08:41:02 +0000

    Add explicit headers for Cloudflare proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,11 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         // Trust Cloudflare proxies for HTTPS detection
-        $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(at: '*', headers:
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
