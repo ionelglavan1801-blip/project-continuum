edd65ae6a23f6d69be97a234bf08edebb3970172
IonG <ionelglavan1801@gmail.com>
2025-12-15 08:41:06 +0000

    Configure Ziggy routes for production URL

---
+++ config/ziggy.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Ziggy URL Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the URL that Ziggy should use when generating
+    | routes. By default, this will be the value of config('app.url').
+    |
+    */
+
+    'url' => 'https://project-continuum.ionglavan.com',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Route Group Middleware
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the middleware that should be applied to
+    | specific route groups. This can be useful if you want to exclude
+    | certain routes from being included in the Ziggy routes.
+    |
+    */
+
+    'groups' => [
+        // 'admin' => ['middleware' => 'admin'],
+    ],
+];
