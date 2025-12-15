170074d508ff651c909f2d2840ac388da2b2eae5
IonG <ionelglavan1801@gmail.com>
2025-12-15 07:23:57 +0000

    Fix TrustProxies configuration to resolve 500 errors on register page

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,14 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         // Trust Cloudflare proxies for HTTPS detection
-        $middleware->trustProxies(
-            at: '*',
-            headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
-                     \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
-                     \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
-                     \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO |
-                     \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB
-        );
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
